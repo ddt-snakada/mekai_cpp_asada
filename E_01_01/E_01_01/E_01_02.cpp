41506ef49c627d55d78269eeb23ed6dedc16b092
ddt-snakada <ddt.snakada@gmail.com>
2020-01-15 19:58:11 +0900

    E_01_02の問題を解き終わりました

---
+++ E_01_01/E_01_01/E_01_02.cpp
@@ -0,0 +1,9 @@
+//list1-1(p.4)のプログラムからusing指令を削除して、coutをstd::coutに変更したプログラムを作成せよ。
+#include<iostream>
+int main()
+{
+	//stdの名前空間を明示し画面への出力を行う
+	std::cout << "初めてのC++プログラム。\n"; 
+	//stdの名前空間を明示し画面への出力を行う
+	std::cout << "画面に出力しています。\n"; 
+}
