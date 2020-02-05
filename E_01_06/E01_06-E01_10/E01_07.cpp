689dedc91eba2a57f36b8d9be8c9042f4265bb30
ddt-snakada <ddt.snakada@gmail.com>
2020-02-05 20:09:44 +0900

    E_01_07の問題が解き終わりました。

---
+++ E_01_06/E01_06-E01_10/E01_07.cpp
@@ -0,0 +1,12 @@
+/*3つのint変数型に値を代入し、それらの合計と平均を求めるプログラムを作成せよ*/
+#include <iostream>
+using namespace std;
+
+int main() {
+	int int_num1 = 5; //int型の変数1つ目
+	int int_num2 = 10;//int型の変数2つ目
+	int int_num3 = 6; //int型の変数3つ目
+
+	cout << int_num1 + int_num2 + int_num3 << "が合計値です\n";     //合計値を表示する
+	cout << (int_num1 + int_num2 + int_num3)/3 << "が平均値です\n"; //平均値を表示する
+}
