4d6d9b60719ac9e75ed4cd4a54218d5fd619c55c
ddt-snakada <ddt.snakada@gmail.com>
2020-02-10 19:14:57 +0900

    平均値を小数にするための変数と、平均値を計算した際に小数になる式を追加し考察をしました

--- E_01_06/E01_06-E01_10/E01_07.cpp
+++ E_01_06/E01_06-E01_10/E01_07.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main() {
 	int int_num1 = 5; //int型の変数1つ目
 	int int_num2 = 10;//int型の変数2つ目
-	int int_num3 = 7; //int型の変数3つ目
+	int int_num3 = 6; //int型の変数3つ目
 	int int_num4 = 8; //平均を小数にするためのint型の変数3つ目
 
 	cout << int_num1 + int_num2 + int_num3 << "が合計値です\n";     //合計値を表示する
-	cout << (int_num1 + int_num2 + int_num3)/3 << "が平均値です\n"; //平均値を表示する
-	cout << (int_num1 + int_num2 + int_num4) / 3 << "が平均値です\n"; //平均値を表示する 平均値は切り捨てられる
+	cout << (int_num1 + int_num2 + int_num3) / 3 << "が平均値です\n"; //平均値を表示する
+	cout << (int_num1 + int_num2 + int_num4) / 3 << "が平均値です\n"; //平均値を表示するが小数点以下は切り捨てられる
 }
