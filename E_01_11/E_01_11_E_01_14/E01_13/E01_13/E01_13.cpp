402634ffeacc739d024744f025c2dd143b4129f7
ddt-snakada <ddt.snakada@gmail.com>
2020-02-12 20:30:14 +0900

    変数の名前をキャメルケースに修正し、コメントもしおりの指示の通りに修正しました。

--- E_01_11/E_01_11_E_01_14/E01_13/E01_13/E01_13.cpp
+++ E_01_11/E_01_11_E_01_14/E01_13/E01_13/E01_13.cpp
@@ -1,6 +1,6 @@
 /*以下に示すプログラムを作成せよ
 ・1桁の正の整数値(すなわち1以上9以下の値)をランダムに生成して表示。
-・1桁の負の整数値(すなわち-9以上-以下の値)をランダムに生成して表示。
+・1桁の負の整数値(すなわち-9以上-1以下の値)をランダムに生成して表示。
 ・2桁の正の整数値(すなわち10以上99以下の値)をランダムに生成して表示。
 */
 
@@ -11,15 +11,22 @@ using namespace std;
 
 int main()
 {
-	int positive_one_digit_int;                 //1桁の正の整数値を入れる変数
-	int negative_one_digit_int;                 //1桁の負の整数値を入れる変数
-	int positive_two_digit_int;                 //2桁の正の整数値を入れる変数
+	int positiveOneDigitInteger;                 //1桁の正の整数値を入れる変数
+	int negativeOneDigitInteger;                 //1桁の負の整数値を入れる変数
+	int positiveTwoDigitInteger;                 //2桁の正の整数値を入れる変数
 	srand(time(NULL));                          //rand関数の種を設定する
-	positive_one_digit_int = (rand() % 9) + 1;  //9未満の正の整数値をランダムに生成し1を足して代入する
-	negative_one_digit_int = -(rand() % 9) - 1; //9未満の正の整数値をランダムに生成しマイナスを付与し、1を引き代入する
-	positive_two_digit_int = (rand() % 90) + 10;//90未満の正の整数値をランダム生成し、10を足し代入する
-	
-	cout << "1桁の正の整数値は" << positive_one_digit_int <<"\n";   //1桁の正の整数値を表示
-	cout << "1桁の負の整数値は" << negative_one_digit_int << "\n";  //1桁の負の整数値を表示
-	cout << "2桁の正の整数値は" << positive_two_digit_int << "\n";  //2桁の正の整数値を表示
+
+	//0から8の正の整数値をランダムに生成し1を足して以上9以下の値を作る
+	positiveOneDigitInteger = (rand() % 9) + 1;  
+	//0から8の正の整数値をランダムに生成しマイナスを付与し、1を引き -9以上-1以下の値を作る
+	negativeOneDigitInteger = -(rand() % 9) - 1; 
+	//0から89の正の整数値をランダム生成し、10を足し10以上99以下の値を作る
+	positiveTwoDigitInteger = (rand() % 90) + 10;
+
+	//1桁の正の整数値を表示
+	cout << "1桁の正の整数値は" << positiveOneDigitInteger <<"\n";   
+	//1桁の負の整数値を表示
+	cout << "1桁の負の整数値は" << negativeOneDigitInteger << "\n";  
+	//2桁の正の整数値を表示
+	cout << "2桁の正の整数値は" << positiveTwoDigitInteger << "\n"; 
 }
