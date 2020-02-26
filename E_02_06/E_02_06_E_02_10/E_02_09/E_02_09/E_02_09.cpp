51a81367410b44d895c07204a9a1440d3e844813
ddt-snakada <ddt.snakada@gmail.com>
2020-02-26 21:21:23 +0900

    演習E02_09が終わりました

---
+++ E_02_06/E_02_06_E_02_10/E_02_09/E_02_09/E_02_09.cpp
@@ -0,0 +1,30 @@
+/*右に示すように、二つの整数値を読み込んで、それらの値の差が10以下であれば「それらの差は10以下です。」
+と表示し、そうでなければ「それらの差は１１以上です。」と表示するプログラムを作成せよ。
+*/
+
+#include<iostream>
+int main() {
+	// 整数値を入力するための二つの変数を用意する。
+	int integerNumberA;
+	int integerNumberB;
+	//整数値の差を代入するための変数を用意する。
+	int difference;
+	//整数Aの値を入力する。
+	std::cout << "整数A:";
+	std::cin >> integerNumberA;
+	//整数Bの値を入力する。
+	std::cout << "整数B:";
+	std::cin >> integerNumberB;
+	//条件演算子で整数1と整数2が評価される。
+	//整数1のが大きい場合は整数1-整数2の式がdifferenceに代入され、それ以外の場合は整数2-整数1がdifferenceに代入される。
+	difference = integerNumberA - integerNumberB ? integerNumberA - integerNumberB : integerNumberB - integerNumberA;
+	
+	//差が10以下の場合の出力処理をする。
+	if (difference <= 10) {
+		std::cout << "それらの差は10以下です。";
+	}//差が11以上の場合の出力処理
+	else if(difference >= 11) {
+		std::cout << "それらの差は11以上です。";
+	}
+	
+}
