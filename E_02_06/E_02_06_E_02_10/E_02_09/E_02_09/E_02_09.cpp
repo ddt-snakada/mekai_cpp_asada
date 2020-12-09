af105c0808e3bc33d6335e42aef23430a5040c21
ddt-snakada <ddt.snakada@gmail.com>
2020-12-09 21:12:54 +0900

    コメントを追加しました。integerNumberAとintegerNumberBは1行で宣言するようにしました。

--- E_02_06/E_02_06_E_02_10/E_02_09/E_02_09/E_02_09.cpp
+++ E_02_06/E_02_06_E_02_10/E_02_09/E_02_09/E_02_09.cpp
@@ -4,16 +4,17 @@
 
 #include<iostream>
 int main() {
-	// 整数値を入力するための二つの変数を用意する。
-	int integerNumberA;
-	int integerNumberB;
-	//整数値の差を代入するための変数を用意する。
+	// 整数値を入力するための二つの変数を用意する
+	int integerNumberA, integerNumberB;
+	//整数値の差を代入するための変数を用意する
 	int difference;
-	//整数Aの値を入力する。
+	//整数Aの入力を促す
 	std::cout << "整数A:";
+	//整数Aの値を入力する
 	std::cin >> integerNumberA;
-	//整数Bの値を入力する。
+	//整数Bの入力を促す
 	std::cout << "整数B:";
+	//整数Bの値を入力する
 	std::cin >> integerNumberB;
 	//条件演算子で整数1と整数2が評価される。
 	//整数1のが大きい場合は整数1-整数2の式がdifferenceに代入され、それ以外の場合は整数2-整数1がdifferenceに代入される。
@@ -21,9 +22,11 @@ int main() {
 	
 	//差が10以下の場合の出力処理をする。
 	if (difference <= 10) {
+		//差が10以下であることを出力する
 		std::cout << "それらの差は10以下です。";
 	}//差が11以上の場合の出力処理
 	else if(difference >= 11) {
+		//差が11以上であることを出力する
 		std::cout << "それらの差は11以上です。";
 	}
 	
