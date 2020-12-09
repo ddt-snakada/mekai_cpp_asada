8c6745b233b05febfb5b0631c5afda0c2da7364f
ddt-snakada <ddt.snakada@gmail.com>
2020-12-09 21:11:17 +0900

    コメントを追加しました。realNumber1とrealNumber2は1行で宣言するように修正しました。

--- E_02_06/E_02_06_E_02_10/E_02_07/E_02_07/E_02_07.cpp
+++ E_02_06/E_02_06_E_02_10/E_02_07/E_02_07/E_02_07.cpp
@@ -3,16 +3,17 @@
 #include<iostream>
 
 int main() {
-	//実数値を入力するための二つの変数を用意する。
-	double realNumber1;
-	double realNumber2;
+	//実数値を入力するための変数を2つ用意する。
+	double realNumber1, realNumber2;
 	//大きい実数を代入するための変数を用意する。
 	double bigNumber;
-	//実数1の値を入力する。
+	//実数1の値の入力を促す
 	std::cout << "実数1の値を入力してください:";
-	std::cin >> realNumber1;
-	//実数2の値を入力する。
+	//実数1の値を入力する
+	std::cin >> realNumber1;	
+	//実数2の値の入力を促す
 	std::cout << "実数2の値を入力してください:";
+	//実数2の値を入力する
 	std::cin >> realNumber2;
 	//条件演算子で実数1と実数2が評価され大きい実数がbigNumberに代入される。
 	//問題文に指定されていないが、値が同じ場合は条件がFALSEになり実数2の値が出力される
