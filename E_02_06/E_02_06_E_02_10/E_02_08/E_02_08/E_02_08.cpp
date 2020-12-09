d9468acd555f94742e539f464ba4df1cb9357897
ddt-snakada <ddt.snakada@gmail.com>
2020-12-09 21:12:08 +0900

    コメントを追加しました。integerNumber1とintegerNumber2は1行で宣言するように修正しました。

--- E_02_06/E_02_06_E_02_10/E_02_08/E_02_08/E_02_08.cpp
+++ E_02_06/E_02_06_E_02_10/E_02_08/E_02_08/E_02_08.cpp
@@ -4,15 +4,16 @@
 
 int main() {
 	//整数値を入力するための二つの変数を用意する。
-	int integerNumber1;
-	int integerNumber2;
+	int integerNumber1, integerNumber2;
 	//整数値の差を代入するための変数を用意する。
 	int difference;
-	//整数1の値を入力する。
+	//整数1の入力を促す
 	std::cout << "整数1の値を入力してください:";
+	//整数1の値を入力する
 	std::cin >> integerNumber1;
-	//整数2の値を入力する。
+	//整数2の入力を促す
 	std::cout << "整数2の値を入力してください:";
+	//整数2の値を入力する
 	std::cin >> integerNumber2;
 	//条件演算子で整数1と整数2が評価される。
 	//整数1のが大きい場合は整数1-整数2の式がdifferenceに代入され、それ以外の場合は整数2-整数1がdifferenceに代入される。
